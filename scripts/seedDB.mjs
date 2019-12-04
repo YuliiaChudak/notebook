7a9c3128675c15d492b595ab37f4e879dcf23eac
Juliia Chudak <juliia.chudak@dev-pro.net>
2019-12-04 22:37:45 +0200

    Update DB seeding

--- scripts/seedDB.mjs
+++ scripts/seedDB.mjs
@@ -1,41 +1,36 @@
 import db from '../db';
 
 async function seedDB() {
-  // await db('person').insert([
-  //   {
-  //     'first_name': 'Yuliia',
-  //     'last_name': 'Chudak',
-  //     'patronymic': 'Valentinivna',
-  //     'birthday': '1993-05-13T00:00:00.000Z',
-  //     'occupation': 'UI',
-  //     'is_studying': false,
-  //     'created_at': '2019-11-04T19:51:57.671Z',
-  //     'updated_at': '2019-11-04T19:51:57.671Z',
-  //     'role_id': 2,
-  //   },
-  // ]);
-  //
   await db('role').insert([
-    {
-      'name': 'Relatives',
-    },
-    {
-      'name': 'Friends',
-    },
-    {
-      'name': 'Colleagues',
-    },
+    { 'name': 'Relatives' },
+    { 'name': 'Friends' },
+    { 'name': 'Colleagues' },
   ]);
 
-  // await db('location').insert([
-  //   {
-  //     'country': 'Ukraine',
-  //     'city': 'Kharkiv',
-  //     'address':'23 Lane, 67',
-  //     'person_id': 1,
-  //   }
-  // ]);
+  if (process.env.WITH_DRAFT) {
+    await db('person').insert([
+      {
+        'first_name': 'Yuliia',
+        'last_name': 'Chudak',
+        'patronymic': 'Valentinivna',
+        'birthday': '1993-05-13T00:00:00.000Z',
+        'occupation': 'UI',
+        'is_studying': false,
+        'created_at': '2019-11-04T19:51:57.671Z',
+        'updated_at': '2019-11-04T19:51:57.671Z',
+        'role_id': 2,
+      },
+    ]);
 
+    await db('location').insert([
+      {
+        'country': 'Ukraine',
+        'city': 'Kharkiv',
+        'address':'23 Lane, 67',
+        'person_id': 1,
+      }
+    ]);
+  }
 
   await db.destroy();
 }
